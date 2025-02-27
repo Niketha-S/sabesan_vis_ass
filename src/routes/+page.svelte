380b65ef3b39a51cc9e61f3e9f279aaaad9b3fd9
Niketha-S <nikisen07@gmail.com>
2025-02-27 11:44:03 -0600

    change 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,7 @@
 <a href="https://niketha-s.github.io/sabesan_vis_ass/A1" target="_blank" rel="noopener noreferrer">
   A1
 </a>
+
+<a href="https://niketha-s.github.io/niki-vis-5609-main-2/" target="_blank" rel="noopener noreferrer">
+  A2
+</a>
